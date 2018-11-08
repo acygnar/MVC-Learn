e1ea8a792ce122369c07a9cfcaca196ea9004c6f
acygnar <42324874+acygnar@users.noreply.github.com>
2018-11-08 21:08:19 +0100

    Add files via upload

---
+++ Repository/PersonRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.ViewModels;
+
+namespace WebApplication3.Repository
+{
+    public class PersonRepository
+    {
+
+
+        ///dekalrowanie zmiennych
+        ///TODO: łaczymy sie z baza w tym miejscu  ///robimy tu metode do laczenia z baza
+        public Person GetPersonByID(int perosnId)
+        {
+
+            var person = new Person
+            {
+                id = 1,
+                name = "Adrian",
+                surname = "Cygnar",
+
+            };
+            var person2 = new Person
+            {
+                id = 2,
+                name = "Grzegorz",
+                surname = "Cygnar",
+
+            };
+            return person;
+
+        }
+    }
+}
